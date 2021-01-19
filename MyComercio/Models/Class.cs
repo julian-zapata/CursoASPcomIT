7ca98dde0526ffde0a51d2a62320e6fd1340073a
Julian Gabriel Zapata <julian89cba@gmail.com>
2021-01-19 19:11:05 -0300

    Agregar archivos de proyecto.

---
+++ MyComercio/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyComercio.Models
+{
+    public class Class
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
---
+++ MyComercio/Models/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyComercio.Models
+{
+    public class Persona
+    {
+        public Guid id;
+
+        [Key]
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
